1ccadcda3a3e988a0e3900f24e23fc34379f761f
jovan-dyaz <jvaonam@me.com>
2026-02-02 00:20:48 -0600

    refactor(notes): cleanup deprecated files

--- apps/notes/src/hooks/index.ts
+++ apps/notes/src/hooks/index.ts
@@ -1,5 +1,9 @@
-export { useDebounce, useDebouncedCallback } from '@knowtis/shared-hooks';
-export { useFilteredNotes } from './useFilteredNotes';
 export { useCollaborativeEditor } from './useCollaborativeEditor';
 export { usePresenceBroadcast } from './usePresenceBroadcast';
 export { useActiveCollaborators } from './useActiveCollaborators';
+export {
+  useWebSocketCollaboration,
+  getCollaborationMode,
+  isWebSocketEnabled,
+  type CollaborationMode,
+} from './useWebSocketCollaboration';
