5f1ee10559915320accaaa3f9f94037b125e118b
jovan-dyaz <jvaonam@me.com>
2026-02-02 00:18:44 -0600

    feat(data-access-notes): add notes query hooks

--- libs/data-access/notes/src/index.ts
+++ libs/data-access/notes/src/index.ts
@@ -1,13 +1,11 @@
-export { useNotesStore } from './notes.store';
-export type { NotesStore, NotesState, NotesActions } from './notes.store.types';
-
 export type { Note, CreateNoteInput, UpdateNoteInput } from './note.types';
 export { NoteSchema, CreateNoteSchema, UpdateNoteSchema } from './note.types';
 
 export {
-  filterNotes,
-  sortNotesByUpdated,
-  filterAndSortNotes,
-} from './note.filters';
-
-export { STORAGE_KEY, INITIAL_NOTES } from './notes.constants';
+  useNotes,
+  useNote,
+  useCreateNote,
+  useUpdateNote,
+  useDeleteNote,
+  notesQueryKeys,
+} from './notes.hooks';
